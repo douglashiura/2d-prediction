6f78e365215dd898ccdb1dde4101782e01dabff9
douglas <douglashiura@gmail.com>
2021-05-24 17:16:09 -0300

    feat: weka

--- 2d-prediction/src/com/github/douglashiura/two/d/view/FileSensorData.java
+++ 2d-prediction/src/com/github/douglashiura/two/d/view/FileSensorData.java
@@ -14,28 +14,39 @@ public class FileSensorData implements Listener {
 	private FileOutputStream file;
 
 	public FileSensorData() throws FileNotFoundException {
-		file = new FileOutputStream("data.txt");
+		file = new FileOutputStream("data.csv");
 	}
 
 	@Override
 	public void detection() {
 		try {
 			file.write("\n".getBytes());
+			for (int i = 0; i < sensors.size(); i++) {
+				Sensor sensor = sensors.get(i);
+				if (i == 0) {
+					file.write(String.format("%s", sensor.getLastTouch().getY()).getBytes());
+
+				} else {
+					file.write(String.format(", %s", sensor.getLastTouch().getY()).getBytes());
+
+				}
+			}
+
 		} catch (IOException e1) {
 		}
-		sensors.forEach(sensor -> {
-			try {
-				file.write(String.format("%s_%s	", sensor.getLastTouch().getX(), sensor.getLastTouch().getY())
-						.getBytes());
-			} catch (IOException e) {
-			}
-		});
+
 	}
 
 	public void setSensors(List<Sensor> sensors) throws IOException {
 		this.sensors = sensors;
 		for (int i = 0; i < sensors.size(); i++) {
-			file.write(String.format("sensor_%s	", i).getBytes());
+			if (i == 0) {
+				file.write(String.format("sensor_%s", i).getBytes());
+
+			} else {
+				file.write(String.format(", sensor_%s", i).getBytes());
+
+			}
 		}
 
 	}
--- 2d-prediction/src/com/github/douglashiura/two/d/view/Main.java
+++ 2d-prediction/src/com/github/douglashiura/two/d/view/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		Integer height = 15;
 		FileSensorData listener = new FileSensorData();
-		GridFactory grid = new GridFactory(height, listener, 3, 6, 14).build();
+		GridFactory grid = new GridFactory(height, listener, 3, 11, 14).build();
 		listener.setSensors(grid.getSensors());
 		Ball ball = new Ball(Directions.BELOW, grid.getPoints().get(0));
 		new Thread() {
